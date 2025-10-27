3fb6e7b2052b2fabaf8feafcd1549993243d65dc
Olivia Marcilla <beribey6@outllook.com>
2025-10-27 20:25:58 +0800

    Divide Function

--- calculator_act.js
+++ calculator_act.js
@@ -12,6 +12,11 @@ function subtract(a, b){
 function multiply(a, b){
     return a*b
 }
+
+function divide(a, b){
+    return a*b
+}
 console.log ("Add: ", add(a, b))
 console.log ("Subtract: ", subtract(a, b) )
 console.log ("Multiply: ", multiply(a, b) )
+console.log ("Divide: ", divide(a, b) )
